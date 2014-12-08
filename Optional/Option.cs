3c687779751bd4b613dcee176bc3d041e37017df
unknown <nilu@LAPTOP-NILUR.cph.kraftvaerk.net>
2014-12-08 17:34:35 +0100

    Added XML comments.

--- Optional/Option.cs
+++ Optional/Option.cs
@@ -11,6 +11,9 @@ namespace Optional
         private bool hasValue;
         private T value;
 
+        /// <summary>
+        /// Checks if a value is present.
+        /// </summary>
         public bool HasValue { get { return hasValue; } }
         internal T Value { get { return value; } }
 
@@ -23,11 +26,20 @@ namespace Optional
 
     public static class Option
     {
+        /// <summary>
+        /// Wraps an existing value in an Option&lt;T&gt; instance.
+        /// </summary>
+        /// <param name="value">The value to be wrapped.</param>
+        /// <returns>An Option&lt;T&gt; instance containing the specified value.</returns>
         public static Option<T> Some<T>(T value)
         {
             return new Option<T>(value, true);
         }
 
+        /// <summary>
+        /// Creates an empty Option&lt;T&gt; instance.
+        /// </summary>
+        /// <returns>An empty instance of Option&lt;T&gt;.</returns>
         public static Option<T> None<T>()
         {
             return new Option<T>(default(T), false);
--- Optional/OptionExtensions.cs
+++ Optional/OptionExtensions.cs
@@ -8,6 +8,12 @@ namespace Optional
 {
     public static class OptionExtensions
     {
+        /// <summary>
+        /// Returns the wrapped value if present, and otherwise an alternative value.
+        /// </summary>
+        /// <param name="option">The Option&lt;T&gt; instance.</param>
+        /// <param name="alternative">The alternative value.</param>
+        /// <returns>The wrapped or alternative value.</returns>
         public static T ValueOr<T>(this Option<T> option, T alternative)
         {
             if (option.HasValue)
@@ -18,6 +24,13 @@ namespace Optional
             return alternative;
         }
 
+        /// <summary>
+        /// Evaluates a specified function, based on whether a value is present or not.
+        /// </summary>
+        /// <param name="option">The Option&lt;T&gt; instance.</param>
+        /// <param name="some">The function to evaluate if the value is present.</param>
+        /// <param name="none">The function to evaluate if the value is missing.</param>
+        /// <returns>The result of the evaluated function.</returns>
         public static TResult Match<T, TResult>(this Option<T> option, Func<T, TResult> some, Func<TResult> none)
         {
             if (option.HasValue)
@@ -28,6 +41,12 @@ namespace Optional
             return none();
         }
 
+        /// <summary>
+        /// Evaluates a specified action, based on whether a value is present or not.
+        /// </summary>
+        /// <param name="option">The Option&lt;T&gt; instance.</param>
+        /// <param name="some">The action to evaluate if the value is present.</param>
+        /// <param name="none">The action to evaluate if the value is missing.</param>
         public static void Match<T>(this Option<T> option, Action<T> some, Action none)
         {
             if (option.HasValue)
@@ -38,16 +57,32 @@ namespace Optional
             none();
         }
 
+        /// <summary>
+        /// Wraps a value in an Option&lt;T&gt; instance.
+        /// </summary>
+        /// <param name="value">The value to wrap.</param>
+        /// <returns>The Option&lt;T&gt; instance.</returns>
         public static Option<T> Some<T>(this T value)
         {
             return Option.Some(value);
         }
 
+        /// <summary>
+        /// Creates an empty Option&lt;T&gt; instance from a specified value.
+        /// </summary>
+        /// <param name="value">A value determining the type of the Option&lt;T&gt; instance.</param>
+        /// <returns>The Option&lt;T&gt; instance.</returns>
         public static Option<T> None<T>(this T value)
         {
             return Option.None<T>();
         }
 
+        /// <summary>
+        /// Creates an Option&lt;T&gt; instance from a specified value. 
+        /// If the value is null, the returned Option&lt;T&gt; instance is empty.
+        /// </summary>
+        /// <param name="value">The value to wrap.</param>
+        /// <returns>The Option&lt;T&gt; instance.</returns>
         public static Option<T> SomeNotNull<T>(this T value)
         {
             if (value != null)
@@ -58,6 +93,11 @@ namespace Optional
             return Option.None<T>();
         }
 
+        /// <summary>
+        /// Converts a Nullable&lt;T&gt; to an Option&lt;T&gt; instance.
+        /// </summary>
+        /// <param name="value">The Nullable&lt;T&gt; instance.</param>
+        /// <returns>The Option&lt;T&gt; instance.</returns>
         public static Option<T> ToOption<T>(this Nullable<T> value) where T : struct
         {
             if (value.HasValue)
@@ -68,6 +108,13 @@ namespace Optional
             return Option.None<T>();
         }
 
+        /// <summary>
+        /// Transforms the inner value in an Option&lt;T&gt; instance.
+        /// If the instance is empty, an empty instance is returned.
+        /// </summary>
+        /// <param name="option">The Option&lt;T&gt; instance.</param>
+        /// <param name="mapping">The transformation function.</param>
+        /// <returns>The transformed Option&lt;T&gt; instance.</returns>
         public static Option<TResult> Map<T, TResult>(this Option<T> option, Func<T, TResult> mapping)
         {
             return option.Match(
@@ -76,6 +123,14 @@ namespace Optional
             );
         }
 
+        /// <summary>
+        /// Transforms the inner value in an Option&lt;T&gt; instance
+        /// into another Option&lt;T&gt; instance. The result is flattened, 
+        /// and if either is empty, an empty instance is returned.
+        /// </summary>
+        /// <param name="option">The Option&lt;T&gt; instance.</param>
+        /// <param name="mapping">The transformation function.</param>
+        /// <returns>The transformed Option&lt;T&gt; instance.</returns>
         public static Option<TResult> FlatMap<T, TResult>(this Option<T> option, Func<T, Option<TResult>> mapping)
         {
             return option.Match(
@@ -84,6 +139,13 @@ namespace Optional
             );
         }
 
+        /// <summary>
+        /// Empties an Option&lt;T&gt; instance, if a specified predicate
+        /// is not satisfied.
+        /// </summary>
+        /// <param name="option">The Option&lt;T&gt; instance.</param>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>The filtered Option&lt;T&gt; instance.</returns>
         public static Option<T> Filter<T>(this Option<T> option, Func<T, bool> predicate)
         {
             return option.Match(
