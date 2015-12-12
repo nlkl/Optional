133da6dc0358b4c38750d8eee96837759832a715
nlkl <nils.luck@outlook.com>
2015-12-12 22:27:34 +0100

    A bit of cleanup in sandbox...

--- Optional.Sandbox/Program.cs
+++ Optional.Sandbox/Program.cs
@@ -12,108 +12,10 @@ using System.Threading.Tasks;
 
 namespace Optional.Sandbox
 {
-    public class Person
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-
-        public Option<Address> TryGetAddress()
-        {
-            return new Address
-            {
-                Id = 1,
-                StreetName = "Option Street",
-                StreetNumber = "22",
-                City = "Optionburg",
-                PostalCode = "2222",
-                Country = "Optionland"
-            }.Some();
-        }
-
-        public Task<Option<Address>> TryGetAddressAsync()
-        {
-            return Task.FromResult(TryGetAddress());
-        }
-    }
-
-    public class Address
-    {
-        public int Id { get; set; }
-        public string StreetName { get; set; }
-        public string StreetNumber { get; set; }
-        public string City { get; set; }
-        public string PostalCode { get; set; }
-        public string Country { get; set; }
-    }
-
-    public static class Repository
-    {
-        public static Option<Person> TryGetPerson(int id)
-        {
-            if (id == 1)
-            {
-                return new Person
-                {
-                    Id = 1,
-                    Name = "Jens Jensen",
-                    Age = 32
-                }.Some();
-            }
-
-            return Option.None<Person>();
-        }
-
-        public static Task<Option<Person>> TryGetPersonAsync(int id)
-        {
-            return Task.FromResult(TryGetPerson(id));
-        }
-    }
-
     class Program
     {
-        public static async Task TestAddresses()
-        {
-            //var optionalAddress = Repository
-                //.TryGetPerson(1)
-                //.FlatMap(p => p.TryGetAddress());
-
-            //var optionalAddress4 = await Repository
-                //.TryGetPersonAsync(1)
-                //.FlatMap(person => person.TryGetAddressAsync());
-            //var optionalAddress5 = await Repository
-            
-                //.TryGetPersonAsync(1)
-                //.OptionMap(person => person.TryGetAddress());
-
-            //optionalAddress.Match(
-                //some: addr => Console.WriteLine(addr.Country),
-                //none: () => Console.WriteLine("Not found")
-            //);
-
-            //optionalAddress4.Match(
-                //some: addr => Console.WriteLine(addr.Country),
-                //none: () => Console.WriteLine("Not found")
-            //);
-
-            //optionalAddress5.Match(
-                //some: addr => Console.WriteLine(addr.Country),
-                //none: () => Console.WriteLine("Not found")
-            //);
-        }
-        
-        //public static AsyncOption<int> Something()
-        //{
-        //    return AsyncOption.FromTask(async () =>
-        //    {
-        //        return Option.None<int>();
-        //    });
-        //}
-
         static void Main(string[] args)
         {
-            TestAddresses().Wait();
-
             Console.Read();
         }
     }
