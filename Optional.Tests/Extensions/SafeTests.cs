ad888c9811b83a6d7e6323b43cc00a2e542f4f6b
Nils Lück <nils.luck@outlook.com>
2016-04-17 14:01:18 +0200

    Renamed Try to Safe, and Run to Catch

--- Optional.Tests/Extensions/SafeTests.cs
+++ Optional.Tests/Extensions/SafeTests.cs
@@ -21,16 +21,16 @@ using Optional.Tests.Utilities;
 namespace Optional.Tests.Extensions
 {
     [TestClass]
-    public class TryTests
+    public class SafeTests
     {
         [TestMethod]
-        public void Extensions_Try_CatchAll()
+        public void Extensions_Safe_CatchAll()
         {
             var ex1 = new Ex("ex");
             var ex2 = new Ex1("ex");
 
-            var error1 = Try.Run<bool>(() => { throw ex1; });
-            var error2 = Try.Run<bool>(() => { throw ex2; });
+            var error1 = Safe.Catch<bool>(() => { throw ex1; });
+            var error2 = Safe.Catch<bool>(() => { throw ex2; });
 
             Assert.IsFalse(error1.HasValue);
             Assert.IsFalse(error2.HasValue);
@@ -41,12 +41,12 @@ namespace Optional.Tests.Extensions
             Assert.AreEqual(error1.Match(x => null, ex => ex), ex1);
             Assert.AreEqual(error2.Match(x => null, ex => ex), ex2);
 
-            var success = Try.Run(() => true);
+            var success = Safe.Catch(() => true);
             Assert.IsTrue(success.ValueOr(false));
         }
 
         [TestMethod]
-        public void Extensions_Try_Catch1()
+        public void Extensions_Safe_Catch1()
         {
             var ex0 = new Ex("ex");
             var ex1 = new Ex1("ex");
@@ -54,25 +54,25 @@ namespace Optional.Tests.Extensions
             var baseEx = new BaseEx("ex");
             var subEx = new SubEx("ex");
 
-            var err1 = Try.Run<bool, Ex1>(() => { throw ex1; });
+            var err1 = Safe.Catch<bool, Ex1>(() => { throw ex1; });
 
             Assert.IsFalse(err1.HasValue);
             Assert.IsInstanceOfType(err1.Match(x => null, ex => ex), typeof(Ex1));
             Assert.AreEqual(err1.Match(x => null, ex => ex), ex1);
 
-            CustomAssert.Throws<Ex>(() => Try.Run<bool, Ex2>(() => { throw ex0; }));
-            CustomAssert.Throws<Ex1>(() => Try.Run<bool, Ex2>(() => { throw ex1; }));
+            CustomAssert.Throws<Ex>(() => Safe.Catch<bool, Ex2>(() => { throw ex0; }));
+            CustomAssert.Throws<Ex1>(() => Safe.Catch<bool, Ex2>(() => { throw ex1; }));
 
-            Try.Run<bool, BaseEx>(() => { throw subEx; });
-            Try.Run<bool, Ex>(() => { throw ex0; });
-            Try.Run<bool, Ex>(() => { throw ex1; });
+            Safe.Catch<bool, BaseEx>(() => { throw subEx; });
+            Safe.Catch<bool, Ex>(() => { throw ex0; });
+            Safe.Catch<bool, Ex>(() => { throw ex1; });
 
-            var success = Try.Run<bool, Ex1>(() => true);
+            var success = Safe.Catch<bool, Ex1>(() => true);
             Assert.IsTrue(success.ValueOr(false));
         }
 
         [TestMethod]
-        public void Extensions_Try_Catch2()
+        public void Extensions_Safe_Catch2()
         {
             var ex0 = new Ex("ex");
             var ex1 = new Ex1("ex");
@@ -81,8 +81,8 @@ namespace Optional.Tests.Extensions
             var baseEx = new BaseEx("ex");
             var subEx = new SubEx("ex");
 
-            var err1 = Try.Run<bool, Ex1, Ex2>(() => { throw ex1; });
-            var err2 = Try.Run<bool, Ex1, Ex2>(() => { throw ex2; });
+            var err1 = Safe.Catch<bool, Ex1, Ex2>(() => { throw ex1; });
+            var err2 = Safe.Catch<bool, Ex1, Ex2>(() => { throw ex2; });
 
             Assert.IsFalse(err1.HasValue);
             Assert.IsFalse(err2.HasValue);
@@ -93,19 +93,19 @@ namespace Optional.Tests.Extensions
             Assert.AreEqual(err1.Match(x => null, ex => ex), ex1);
             Assert.AreEqual(err2.Match(x => null, ex => ex), ex2);
 
-            CustomAssert.Throws<Ex>(() => Try.Run<bool, Ex2, Ex3>(() => { throw ex0; }));
-            CustomAssert.Throws<Ex1>(() => Try.Run<bool, Ex2, Ex3>(() => { throw ex1; }));
+            CustomAssert.Throws<Ex>(() => Safe.Catch<bool, Ex2, Ex3>(() => { throw ex0; }));
+            CustomAssert.Throws<Ex1>(() => Safe.Catch<bool, Ex2, Ex3>(() => { throw ex1; }));
 
-            Try.Run<bool, BaseEx, Ex2>(() => { throw subEx; });
-            Try.Run<bool, Ex, Ex2>(() => { throw ex0; });
-            Try.Run<bool, Ex, Ex2>(() => { throw ex1; });
+            Safe.Catch<bool, BaseEx, Ex2>(() => { throw subEx; });
+            Safe.Catch<bool, Ex, Ex2>(() => { throw ex0; });
+            Safe.Catch<bool, Ex, Ex2>(() => { throw ex1; });
 
-            var success = Try.Run<bool, Ex1, Ex2>(() => true);
+            var success = Safe.Catch<bool, Ex1, Ex2>(() => true);
             Assert.IsTrue(success.ValueOr(false));
         }
 
         [TestMethod]
-        public void Extensions_Try_Catch3()
+        public void Extensions_Safe_Catch3()
         {
             var ex0 = new Ex("ex");
             var ex1 = new Ex1("ex");
@@ -115,9 +115,9 @@ namespace Optional.Tests.Extensions
             var baseEx = new BaseEx("ex");
             var subEx = new SubEx("ex");
 
-            var err1 = Try.Run<bool, Ex1, Ex2, Ex3>(() => { throw ex1; });
-            var err2 = Try.Run<bool, Ex1, Ex2, Ex3>(() => { throw ex2; });
-            var err3 = Try.Run<bool, Ex1, Ex2, Ex3>(() => { throw ex3; });
+            var err1 = Safe.Catch<bool, Ex1, Ex2, Ex3>(() => { throw ex1; });
+            var err2 = Safe.Catch<bool, Ex1, Ex2, Ex3>(() => { throw ex2; });
+            var err3 = Safe.Catch<bool, Ex1, Ex2, Ex3>(() => { throw ex3; });
 
             Assert.IsFalse(err1.HasValue);
             Assert.IsFalse(err2.HasValue);
@@ -131,19 +131,19 @@ namespace Optional.Tests.Extensions
             Assert.AreEqual(err2.Match(x => null, ex => ex), ex2);
             Assert.AreEqual(err3.Match(x => null, ex => ex), ex3);
 
-            CustomAssert.Throws<Ex>(() => Try.Run<bool, Ex2, Ex3, Ex4>(() => { throw ex0; }));
-            CustomAssert.Throws<Ex1>(() => Try.Run<bool, Ex2, Ex3, Ex4>(() => { throw ex1; }));
+            CustomAssert.Throws<Ex>(() => Safe.Catch<bool, Ex2, Ex3, Ex4>(() => { throw ex0; }));
+            CustomAssert.Throws<Ex1>(() => Safe.Catch<bool, Ex2, Ex3, Ex4>(() => { throw ex1; }));
 
-            Try.Run<bool, BaseEx, Ex2, Ex3>(() => { throw subEx; });
-            Try.Run<bool, Ex, Ex2, Ex3>(() => { throw ex0; });
-            Try.Run<bool, Ex, Ex2, Ex3>(() => { throw ex1; });
+            Safe.Catch<bool, BaseEx, Ex2, Ex3>(() => { throw subEx; });
+            Safe.Catch<bool, Ex, Ex2, Ex3>(() => { throw ex0; });
+            Safe.Catch<bool, Ex, Ex2, Ex3>(() => { throw ex1; });
 
-            var success = Try.Run<bool, Ex1, Ex2, Ex3>(() => true);
+            var success = Safe.Catch<bool, Ex1, Ex2, Ex3>(() => true);
             Assert.IsTrue(success.ValueOr(false));
         }
 
         [TestMethod]
-        public void Extensions_Try_Catch4()
+        public void Extensions_Safe_Catch4()
         {
             var ex0 = new Ex("ex");
             var ex1 = new Ex1("ex");
@@ -154,10 +154,10 @@ namespace Optional.Tests.Extensions
             var baseEx = new BaseEx("ex");
             var subEx = new SubEx("ex");
 
-            var err1 = Try.Run<bool, Ex1, Ex2, Ex3, Ex4>(() => { throw ex1; });
-            var err2 = Try.Run<bool, Ex1, Ex2, Ex3, Ex4>(() => { throw ex2; });
-            var err3 = Try.Run<bool, Ex1, Ex2, Ex3, Ex4>(() => { throw ex3; });
-            var err4 = Try.Run<bool, Ex1, Ex2, Ex3, Ex4>(() => { throw ex4; });
+            var err1 = Safe.Catch<bool, Ex1, Ex2, Ex3, Ex4>(() => { throw ex1; });
+            var err2 = Safe.Catch<bool, Ex1, Ex2, Ex3, Ex4>(() => { throw ex2; });
+            var err3 = Safe.Catch<bool, Ex1, Ex2, Ex3, Ex4>(() => { throw ex3; });
+            var err4 = Safe.Catch<bool, Ex1, Ex2, Ex3, Ex4>(() => { throw ex4; });
 
             Assert.IsFalse(err1.HasValue);
             Assert.IsFalse(err2.HasValue);
@@ -174,19 +174,19 @@ namespace Optional.Tests.Extensions
             Assert.AreEqual(err3.Match(x => null, ex => ex), ex3);
             Assert.AreEqual(err4.Match(x => null, ex => ex), ex4);
 
-            CustomAssert.Throws<Ex>(() => Try.Run<bool, Ex2, Ex3, Ex4, Ex5>(() => { throw ex0; }));
-            CustomAssert.Throws<Ex1>(() => Try.Run<bool, Ex2, Ex3, Ex4, Ex5>(() => { throw ex1; }));
+            CustomAssert.Throws<Ex>(() => Safe.Catch<bool, Ex2, Ex3, Ex4, Ex5>(() => { throw ex0; }));
+            CustomAssert.Throws<Ex1>(() => Safe.Catch<bool, Ex2, Ex3, Ex4, Ex5>(() => { throw ex1; }));
 
-            Try.Run<bool, BaseEx, Ex2, Ex3, Ex4>(() => { throw subEx; });
-            Try.Run<bool, Ex, Ex2, Ex3, Ex4>(() => { throw ex0; });
-            Try.Run<bool, Ex, Ex2, Ex3, Ex4>(() => { throw ex1; });
+            Safe.Catch<bool, BaseEx, Ex2, Ex3, Ex4>(() => { throw subEx; });
+            Safe.Catch<bool, Ex, Ex2, Ex3, Ex4>(() => { throw ex0; });
+            Safe.Catch<bool, Ex, Ex2, Ex3, Ex4>(() => { throw ex1; });
 
-            var success = Try.Run<bool, Ex1, Ex2, Ex3, Ex4>(() => true);
+            var success = Safe.Catch<bool, Ex1, Ex2, Ex3, Ex4>(() => true);
             Assert.IsTrue(success.ValueOr(false));
         }
 
         [TestMethod]
-        public void Extensions_Try_Catch5()
+        public void Extensions_Safe_Catch5()
         {
             var ex0 = new Ex("ex");
             var ex1 = new Ex1("ex");
@@ -198,11 +198,11 @@ namespace Optional.Tests.Extensions
             var baseEx = new BaseEx("ex");
             var subEx = new SubEx("ex");
 
-            var err1 = Try.Run<bool, Ex1, Ex2, Ex3, Ex4, Ex5>(() => { throw ex1; });
-            var err2 = Try.Run<bool, Ex1, Ex2, Ex3, Ex4, Ex5>(() => { throw ex2; });
-            var err3 = Try.Run<bool, Ex1, Ex2, Ex3, Ex4, Ex5>(() => { throw ex3; });
-            var err4 = Try.Run<bool, Ex1, Ex2, Ex3, Ex4, Ex5>(() => { throw ex4; });
-            var err5 = Try.Run<bool, Ex1, Ex2, Ex3, Ex4, Ex5>(() => { throw ex5; });
+            var err1 = Safe.Catch<bool, Ex1, Ex2, Ex3, Ex4, Ex5>(() => { throw ex1; });
+            var err2 = Safe.Catch<bool, Ex1, Ex2, Ex3, Ex4, Ex5>(() => { throw ex2; });
+            var err3 = Safe.Catch<bool, Ex1, Ex2, Ex3, Ex4, Ex5>(() => { throw ex3; });
+            var err4 = Safe.Catch<bool, Ex1, Ex2, Ex3, Ex4, Ex5>(() => { throw ex4; });
+            var err5 = Safe.Catch<bool, Ex1, Ex2, Ex3, Ex4, Ex5>(() => { throw ex5; });
 
             Assert.IsFalse(err1.HasValue);
             Assert.IsFalse(err2.HasValue);
@@ -222,14 +222,14 @@ namespace Optional.Tests.Extensions
             Assert.AreEqual(err4.Match(x => null, ex => ex), ex4);
             Assert.AreEqual(err5.Match(x => null, ex => ex), ex5);
 
-            CustomAssert.Throws<Ex>(() => Try.Run<bool, Ex2, Ex3, Ex4, Ex5, Ex6>(() => { throw ex0; }));
-            CustomAssert.Throws<Ex1>(() => Try.Run<bool, Ex2, Ex3, Ex4, Ex5, Ex6>(() => { throw ex1; }));
+            CustomAssert.Throws<Ex>(() => Safe.Catch<bool, Ex2, Ex3, Ex4, Ex5, Ex6>(() => { throw ex0; }));
+            CustomAssert.Throws<Ex1>(() => Safe.Catch<bool, Ex2, Ex3, Ex4, Ex5, Ex6>(() => { throw ex1; }));
 
-            Try.Run<bool, BaseEx, Ex2, Ex3, Ex4, Ex5>(() => { throw subEx; });
-            Try.Run<bool, Ex, Ex2, Ex3, Ex4, Ex5>(() => { throw ex0; });
-            Try.Run<bool, Ex, Ex2, Ex3, Ex4, Ex5>(() => { throw ex1; });
+            Safe.Catch<bool, BaseEx, Ex2, Ex3, Ex4, Ex5>(() => { throw subEx; });
+            Safe.Catch<bool, Ex, Ex2, Ex3, Ex4, Ex5>(() => { throw ex0; });
+            Safe.Catch<bool, Ex, Ex2, Ex3, Ex4, Ex5>(() => { throw ex1; });
 
-            var success = Try.Run<bool, Ex1, Ex2, Ex3, Ex4, Ex5>(() => true);
+            var success = Safe.Catch<bool, Ex1, Ex2, Ex3, Ex4, Ex5>(() => true);
             Assert.IsTrue(success.ValueOr(false));
         }
     }
