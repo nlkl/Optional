0716d71f169e7a71777655613426bab5ab37bb50
Nils Lück <nils.luck@outlook.com>
2016-10-01 14:32:00 +0200

    Minor improvements to build

---
+++ build-clean.cmd
@@ -0,0 +1,7 @@
+@echo off
+RMDIR "Optional\bin" /S /Q
+RMDIR "Optional.Collections\bin" /S /Q
+RMDIR "Optional.Utilities\bin" /S /Q
+RMDIR "Portable.Optional\bin" /S /Q
+RMDIR "Portable.Optional.Collections\bin" /S /Q
+RMDIR "Portable.Optional.Utilities\bin" /S /Q
