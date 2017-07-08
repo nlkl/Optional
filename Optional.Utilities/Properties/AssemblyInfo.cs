a38b52a428521d0518c7f23f1a3bed58fc62edf8
nlkl <nils.luck@outlook.com>
2017-07-08 19:11:03 +0200

    Incremented versions for future release

--- Optional.Utilities/Properties/AssemblyInfo.cs
+++ Optional.Utilities/Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
