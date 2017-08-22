8476fe6300404c2e9bf2c520f47097d629aeb32d
nlkl <nils.luck@outlook.com>
2017-08-22 21:23:22 +0200

    Moved Optional.Collections into a Sample project instead

--- src/Optional.Samples/LinqQueryableExtensions.cs
+++ src/Optional.Samples/LinqQueryableExtensions.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 
-namespace Optional.Collections
+namespace Optional.Samples
 {
     public static class LinqQueryableExtensions
     {
