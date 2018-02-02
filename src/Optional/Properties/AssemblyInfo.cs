5901a9a8680020abea27e9991d10142b4338afc1
nlkl <nils.luck@outlook.com>
2018-02-02 22:07:50 +0100

    Changed version number to 4.1.0

--- src/Optional/Properties/AssemblyInfo.cs
+++ src/Optional/Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
