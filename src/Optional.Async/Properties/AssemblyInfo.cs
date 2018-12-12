3e8b3ee5609cf3d0862cdb61f4488c9931993c53
nlkl <nils.luck@outlook.com>
2018-12-12 22:10:39 +0100

    Bump Optional.Async version to 0.2.0

--- src/Optional.Async/Properties/AssemblyInfo.cs
+++ src/Optional.Async/Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
